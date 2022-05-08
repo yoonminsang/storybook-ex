d7e2d3a8c92af9c4827f80d6db1bc16a5fd55d74
yoonminsnag <apdlyooapdl@naver.com>
2022-05-08 19:09:10 +0900

    feat: inboxScreen 스토리 변경
    - msw를 이용한 api 통신
    - play 옵션을 통한 자동 유저이벤트

--- src/components/InboxScreen.stories.js
+++ src/components/InboxScreen.stories.js
@@ -1,31 +1,64 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { action } from '@storybook/addon-actions';
-import { PureInboxScreen } from './InboxScreen';
-import * as TaskListStories from './TaskList.stories';
 
-// A super-simple mock of a redux store
-const store = {
-  getState: () => {
-    return {
-      task: { tasks: TaskListStories.Default.args.tasks, error: null },
-    };
-  },
-  subscribe: () => 0,
-  dispatch: action('dispatch'),
-};
+import InboxScreen from './InboxScreen';
+import store from '../lib/store';
+import { rest } from 'msw';
+import { MockedState } from './TaskList.stories';
+
+import { Provider } from 'react-redux';
+
+import {
+  fireEvent,
+  within,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@storybook/testing-library';
 
 export default {
-  component: PureInboxScreen,
-  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
+  component: InboxScreen,
   title: 'InboxScreen',
+  decorators: [(story) => <Provider store={store}>{story()}</Provider>],
 };
 
-const Template = (args) => <PureInboxScreen {...args} />;
+const Template = () => <InboxScreen />;
 
 export const Default = Template.bind({});
+Default.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=1',
+        (req, res, ctx) => {
+          return res(ctx.json(MockedState.tasks));
+        }
+      ),
+    ],
+  },
+};
 
 export const Error = Template.bind({});
-Error.args = {
-  error: 'Something',
+Error.parameters = {
+  msw: {
+    handlers: [
+      rest.get(
+        'https://jsonplaceholder.typicode.com/todos?userId=1',
+        (req, res, ctx) => {
+          return res(ctx.status(403));
+        }
+      ),
+    ],
+  },
+};
+
+Default.play = async ({ canvasElement }) => {
+  const canvas = within(canvasElement);
+  // Waits for the component to transition from the loading state
+  await waitForElementToBeRemoved(await canvas.findByTestId('loading'));
+  // Waits for the component to be updated based on the store
+  await waitFor(async () => {
+    // Simulates pinning the first task
+    await fireEvent.click(canvas.getByLabelText('pinTask-1'));
+    // Simulates pinning the third task
+    await fireEvent.click(canvas.getByLabelText('pinTask-3'));
+  });
 };
