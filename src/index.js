75b9081bf5eb7f913567b50904e0f1473149278a
yoonminsnag <apdlyooapdl@naver.com>
2022-05-08 18:33:05 +0900

    feat: 변경된 스토어 연결

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { configureStore } from '@reduxjs/toolkit';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import rootReducer from './lib/store';
-
-const store = configureStore({
-  reducer: rootReducer,
-});
+import store from './lib/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
